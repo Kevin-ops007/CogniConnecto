8a60c7eab3d80ce5241d2fbff3e3a0410449f720
ZsH+9bqWNTiCOPPER7.3.pyHf <omarmoh272001@gmail.com>
2024-01-27 15:40:56 -0800

    fix: fixed the ID duplication in tasks

--- frontend/src/Components/Tasks/Tasks.js
+++ frontend/src/Components/Tasks/Tasks.js
@@ -14,9 +14,50 @@ function Tasks() {
         const [modealinputDate, setModealinputDate] = useState("");
 
         const [IDs, setIDs] = useState([
+            "1",
+            "2",
+            "3",
+            "4",
         ]);
         const [tasks, setTasks] = useState({
+            "1":{
+                "id": "1",
+                "name": "Task 1",
+                "description": "Description 1",
+                "date": "2021-10-01",
+                "time": "10:00",
+                "completed": false,
+                "show": true
+            },
+            "2":{
+                "id": "2",
+                "name": "Task 2",
+                "description": "Description 2",
+                "date": "2021-10-02",
+                "time": "11:00",
+                "completed": false,
+                "show": true
+            },
+            "3":{
+                "id": "3",
+                "name": "Task 3",
+                "description": "Description 3",
+                "date": "2021-10-03",
+                "time": "12:00",
+                "completed": false,
+                "show": true
+            },
+            "4":{
+                "id": "4",
+                "name": "Task 4",
+                "description": "Description 4",
+                "date": "2021-10-04",
+                "time": "13:00",
+                "completed": false,
+                "show": true
+            },
         });
+        
         useEffect(() => {
             let formattedTasks = JSON.stringify(tasks);
             localStorage.setItem("Tasks", formattedTasks);
@@ -50,7 +91,10 @@ function Tasks() {
 
         const CreateTask = () => {
             let old_IDs = [...IDs];
-            const new_ID = old_IDs.length + 1;
+            let new_ID = old_IDs.length + 1;
+            while (IDs.includes(new_ID)){
+                new_ID++;
+            }
             const new_IDs = [...old_IDs, String(new_ID)];
             setIDs(new_IDs);
 
@@ -124,18 +168,18 @@ function Tasks() {
             <Row className="justify-content-md-center" key={task_id}>
             <Col md={6} className="mb-2">
               <Button onClick={() =>{toggleShowA(task_id)}} variant="outline-dark">{tasks[task_id].name}</Button>
-              <Toast onClose={() =>{toggleShowA(task_id)}} show={tasks[task_id].show} animation={false}>
+              <Toast fluid onClose={() =>{toggleShowA(task_id)}} show={tasks[task_id].show} animation={false}>
                 <Toast.Header>
                   <img
                     src="holder.js/20x20?text=%20"
                     className="rounded me-2"
                     alt=""
                   />
-                  <strong className="me-auto">Bootstrap</strong>
+                  <strong className="me-auto">{tasks[task_id].name}</strong>
                   <small>{tasks[task_id].completed}</small>
                 </Toast.Header>
                 <Toast.Body>{tasks[task_id].description}</Toast.Body>
-                <Button variant="outline-danger" size="sm" onClick={()=>{DeleteTask(task_id)}}>Delete Task</Button>{' '}
+                <Button variant="outline-danger" className="justify-content-md-center" size="sm" onClick={()=>{DeleteTask(task_id)}}>Delete Task</Button>{' '}
               </Toast>
             </Col>
             
