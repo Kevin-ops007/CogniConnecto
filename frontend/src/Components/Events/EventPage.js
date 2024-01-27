d4f463ec3b67f1cb901c17cb4244ba55c6426bcd
kevin <kmathew@peloton-technologies.com>
2024-01-27 12:24:51 -0800

    events

--- frontend/src/Components/Events/EventPage.js
+++ frontend/src/Components/Events/EventPage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "../Header/Header";
-
+import Event from "./Event";
 const EventPage = () => {
     return (
         <>
             <Header />
+            <Event />
         </>
     );
 };
