2d7c50dac0b5f8b3f03f393c6aafccacd3900e5f
ZsH+9bqWNTiCOPPER7.3.pyHf <omarmoh272001@gmail.com>
2024-01-27 10:40:42 -0800

    feat: Routes updates, addindg a dependency

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Tasks from './Components/Tasks/Tasks';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
       <Routes>
-        {<Route path="/" element={<App />}></Route>}
+        <Route path="/" element={<App />}></Route>
+        <Route path="/tasks" element={<Tasks />}></Route>
       </Routes>
     </BrowserRouter>
 );
