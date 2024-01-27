c33befb8f4f38ef1cf79b240e81154895a5be46b
poomon002 <poomon00134089@hotmail.com>
2024-01-27 10:56:06 -0800

    button

---
+++ frontend/src/Components/Buttons/JobButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+const JobDetails = () => {
+  return (
+    <>
+      <h1>Job Details</h1>
+      <Button variant="primary">Apply</Button>
+    </>
+  );
+};
+
+export default JobDetails;
---
+++ frontend/src/Components/Jobs/JobDetails.js
@@ -0,0 +1,11 @@
+import JobButton from "../Buttons/JobButton";
+const JobDetails = () => {
+  return (
+    <>
+      <h1>Job Details</h1>
+      <JobButton />
+    </>
+  );
+};
+
+export default JobDetails;
