d544f277aaf9605176e4ba36b56a521a46509ad3
kevin <kmathew@peloton-technologies.com>
2024-01-27 10:46:20 -0800

    header

---
+++ frontend/src/Components/Header/Header.js
@@ -0,0 +1,40 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <Navbar expand="lg" className="bg-white mb-3" sticky="top">
+            <Container fluid>
+                <Navbar.Brand href="#" as="h1">
+                    CogniConnecto
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Offcanvas placement="end">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>CogniConnecto</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                            <Link to="/events" className="nav-link">
+                                Events
+                            </Link>
+                            <Link to="/jobs" className="nav-link">
+                                Jobs
+                            </Link>
+                            <Link to="/tasks" className="nav-link">
+                                Tasks
+                            </Link>
+                            <Link to="/" className="btn btn-outline-primary">
+                                Sign out
+                            </Link>
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
+    );
+}
+export default Navigation;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Tasks from './Components/Tasks/Tasks';
+import EventPage from './Components/Events/EventPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-        <Route path="/tasks" element={<Tasks />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/tasks" element={<Tasks />}></Route>
+      <Route path="/events" element={<EventPage />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 
